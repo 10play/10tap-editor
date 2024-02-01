28402f59c155410503f3c728c64d185f0d917566
GuySerfaty <guy353@gmail.com>
2024-02-01 18:53:08 +0200

    fix: css on android

--- src/RichText/RichText.tsx
+++ src/RichText/RichText.tsx
@@ -88,28 +88,6 @@ export const RichText = (props: RichTextProps) => {
             `
           : undefined
       }
-      injectedJavaScriptBeforeContentLoaded={
-        editor.plugins
-          ? `
-          window.whiteListPlugins = [${editor.plugins
-            .map((plugin) => `'${plugin.name}'`)
-            .join(',')}];
-              var css = 'p { background: red; }',
-              head = document.head || document.getElementsByTagName('head')[0],
-              style = document.createElement('style');
-
-          head.appendChild(style);
-
-          style.type = 'text/css';
-          if (style.styleSheet){
-            // This is required for IE8 and below.
-            style.styleSheet.cssText = css;
-          } else {
-            style.appendChild(document.createTextNode(css));
-          }
-              `
-          : undefined
-      }
       hideKeyboardAccessoryView={true}
       onMessage={onWebviewMessage}
       ref={props.editor.webviewRef}
