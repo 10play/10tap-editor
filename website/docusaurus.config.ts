555d8fa175472611809c9493fb8d654d181becf5
GuySerfaty <guy353@gmail.com>
2024-02-04 15:52:16 +0200

    fix: change dac name

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -15,8 +15,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: '10play', // Usually your GitHub org/user name.
+  projectName: '10tap', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
