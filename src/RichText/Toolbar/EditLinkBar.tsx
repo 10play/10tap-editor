68da51ba25eff5a14cc2ff02d9a173f52e88ced7
GuySerfaty <guy353@gmail.com>
2024-02-05 18:54:00 +0200

    fix: improve add link style

--- src/RichText/Toolbar/EditLinkBar.tsx
+++ src/RichText/Toolbar/EditLinkBar.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import {
+  View,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+} from 'react-native';
 
 const toolbarStyles = StyleSheet.create({
   addLinkContainer: {
     flex: 1,
     flexDirection: 'row',
+
+    height: 40,
   },
   linkInput: {
     paddingLeft: 12,
+    paddingTop: 1,
+    paddingBottom: 1,
     paddingRight: 12,
+    borderTopWidth: 1,
+    borderTopColor: '#e0e0e0',
     flex: 1,
+  },
+  doneButton: {
+    backgroundColor: '#e7eefc',
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderBottomWidth: 0,
+    borderRightWidth: 0,
+    borderColor: '#e0e0e0',
+    justifyContent: 'center',
+  },
+  doneButtonText: {
+    color: '#376eab',
   },
 });
 
@@ -35,12 +59,14 @@ export const EditLinkBar = ({
         autoFocus
         style={toolbarStyles.linkInput}
       />
-      <Button
-        title="DONE"
+      <TouchableOpacity
+        style={toolbarStyles.doneButton}
         onPress={() => {
           onEditLink(link);
         }}
-      />
+      >
+        <Text style={toolbarStyles.doneButtonText}>Done</Text>
+      </TouchableOpacity>
     </View>
   );
 };
