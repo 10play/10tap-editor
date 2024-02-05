08b0e41e96bdef886398ab05d377d18430a0eb29
Amir Angel <36531255+17Amir17@users.noreply.github.com>
2024-02-05 15:10:38 +0200

    fix: fix toolbar shown when editor not focused

--- src/RichText/Toolbar/Toolbar.tsx
+++ src/RichText/Toolbar/Toolbar.tsx
@@ -64,7 +64,7 @@ export function Toolbar({
   );
 
   const hideToolbar =
-    hidden || (keyboardAware && !isKeyboardUp && !editorState.isFocused);
+    hidden || (keyboardAware && (!isKeyboardUp || !editorState.isFocused));
 
   const args = {
     editor,
