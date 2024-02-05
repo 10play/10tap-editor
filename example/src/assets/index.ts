e9e7d2aa3bef540bcb326d297d35faafc490e33f
Amir Angel <36531255+17Amir17@users.noreply.github.com>
2024-02-05 19:27:50 +0200

    feat: custom toolbar

--- example/src/assets/index.ts
+++ example/src/assets/index.ts
@@ -1,7 +1,13 @@
 import close from './close.svg';
 import send from './send.svg';
+import formatting from './formatting.svg';
+import bold from './bold.svg';
+import h1 from './h1.svg';
 
 export const icons = {
   close,
   send,
+  formatting,
+  bold,
+  h1,
 };
