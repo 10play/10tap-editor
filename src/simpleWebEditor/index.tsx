56e9a5061687e22d78eca78e3206a7c80bb4a7fe
GuySerfaty <guy353@gmail.com>
2024-02-05 12:53:46 +0200

    fix: test

--- src/simpleWebEditor/index.tsx
+++ src/simpleWebEditor/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Tiptap from './Tiptap';
 
+// check
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(<Tiptap />);
