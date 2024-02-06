7dc029fcb177341c3d4c3c48902be301d87468bb
GuySerfaty <guy353@gmail.com>
2024-02-06 16:37:31 +0200

    fix: web

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { Redirect } from '@docusaurus/router';
 
 export default function Home(): JSX.Element {
-  return <Redirect to="/10Tap-Editor/docs/intro" />;
+  return <Redirect to="/10tap-editor/docs/intro" />;
 }
