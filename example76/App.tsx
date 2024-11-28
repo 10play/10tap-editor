afc6426ca7267ea41f1c8c63fa8b91ab5e0d384d
GuySerfaty <guy353@gmail.com>
2024-11-28 11:47:01 +0200

    fix: add react native 76 example

---
+++ example76/App.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  useWindowDimensions,
+} from 'react-native';
+import {createStaticNavigation} from '@react-navigation/native';
+
+import {RichText, Toolbar, useEditorBridge} from '@10play/tentap-editor';
+
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+
+export const Basic = () => {
+  const editor = useEditorBridge({
+    autofocus: true,
+    avoidIosKeyboard: true,
+    initialContent,
+  });
+
+  const {top} = useSafeAreaInsets();
+  const {width, height} = useWindowDimensions();
+  const isLandscape = width > height;
+  const headerHeight = isLandscape ? 32 : 44;
+  const keyboardVerticalOffset = headerHeight + top;
+
+  return (
+    <SafeAreaView style={exampleStyles.fullScreen}>
+      {/* <Text>s2111s223s</Text> */}
+      {/* <WebView source={{html: '<p>apapzpapa</p>'}} /> */}
+      <RichText editor={editor} />
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        keyboardVerticalOffset={keyboardVerticalOffset}
+        style={exampleStyles.keyboardAvoidingView}>
+        <Toolbar editor={editor} />
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+};
+
+const exampleStyles = StyleSheet.create({
+  fullScreen: {
+    flex: 1,
+  },
+  keyboardAvoidingView: {
+    position: 'absolute',
+    width: '100%',
+    bottom: 0,
+  },
+});
+
+const initialContent = '<p>This is a basic 22example!</p>';
+
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Home: Basic,
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
+
+function App() {
+  return <Navigation />;
+}
+
+export default App;
