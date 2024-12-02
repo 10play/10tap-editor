29f72c4a5696bc843367363d3acb44833c5851d1
Amir Angel <36531255+17Amir17@users.noreply.github.com>
2024-12-02 14:09:46 +0200

    fix: update example 76

--- example76/App.tsx
+++ example76/App.tsx
@@ -1,37 +1,25 @@
+import type {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
   KeyboardAvoidingView,
   Platform,
-  useWindowDimensions,
+  StyleSheet,
 } from 'react-native';
-import {createStaticNavigation} from '@react-navigation/native';
-
 import {RichText, Toolbar, useEditorBridge} from '@10play/tentap-editor';
 
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
-
-export const Basic = () => {
+export const App = ({}: NativeStackScreenProps<any, any, any>) => {
   const editor = useEditorBridge({
     autofocus: true,
     avoidIosKeyboard: true,
     initialContent,
   });
 
-  const {top} = useSafeAreaInsets();
-  const {width, height} = useWindowDimensions();
-  const isLandscape = width > height;
-  const headerHeight = isLandscape ? 32 : 44;
-  const keyboardVerticalOffset = headerHeight + top;
-
   return (
     <SafeAreaView style={exampleStyles.fullScreen}>
       <RichText editor={editor} />
       <KeyboardAvoidingView
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        keyboardVerticalOffset={keyboardVerticalOffset}
         style={exampleStyles.keyboardAvoidingView}>
         <Toolbar editor={editor} />
       </KeyboardAvoidingView>
@@ -50,18 +38,6 @@ const exampleStyles = StyleSheet.create({
   },
 });
 
-const initialContent = '<p>This is a basic example!</p>';
-
-const RootStack = createNativeStackNavigator({
-  screens: {
-    Home: Basic,
-  },
-});
-
-const Navigation = createStaticNavigation(RootStack);
-
-function App() {
-  return <Navigation />;
-}
+const initialContent = `<p>This is a basic example!</p>`;
 
 export default App;
