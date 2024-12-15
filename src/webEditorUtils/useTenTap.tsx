040f89c4f914a0a1b254bdb629d9cadc568c3960
Amir Angel <36531255+17Amir17@users.noreply.github.com>
2024-12-15 15:25:58 +0200

    fix: cleanup

--- src/webEditorUtils/useTenTap.tsx
+++ src/webEditorUtils/useTenTap.tsx
@@ -149,7 +149,6 @@ export const useTenTap = (options?: useTenTapArgs) => {
               payload: height + paragraphHeight,
             });
           }
-          // document.body.style.overflow = 'hidden';
           sendMessage({
             type: CoreEditorActionType.DocumentHeight,
             payload: height,
